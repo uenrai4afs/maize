41e9c34fca2712c72c99468a55c2ed76c4b91e37
uenrai4afs <141588435+uenrai4afs@users.noreply.github.com>
2023-08-16 12:41:19 +0000

    Add files via upload

--- leafclassify.py
+++ leafclassify.py
@@ -41,11 +41,11 @@ set_background('maize.png')
 
 def main():
     file_uploaded = st.file_uploader("Choose File", type=["png", "jpg", "jpeg"])
-    class_btn = st.button("Classify")
+    
     if file_uploaded is not None:
         image = Image.open(file_uploaded)
         st.image(image, caption='Uploaded Image', use_column_width=False)
-
+    class_btn = st.button("Detect")
     if class_btn:
         if file_uploaded is None:
             st.write("Invalid command, please upload an image")
@@ -57,7 +57,7 @@ def main():
                 predictions = predict(image)
 
                 time.sleep(1)
-                st.success('Classified')
+                st.success('Detect')
                 st.write(predictions)
 
 
@@ -128,8 +128,7 @@ def predict(image):
     high=np.argmax(probabilities)
     result_1=label_new[high]
     confidence=100 * np.max(probabilities)
-    result=str(result_1) +"     "+ "  "+ str(confidence)
-
+    result="Category:"+ "  "+str(result_1) +"     "+ "\nConfidence: "+ " "+ str(confidence)+ "%"
 
     return result
 
